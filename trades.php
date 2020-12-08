93382e640f2af234dc0bb29fc919821cf477649b
Kori Vernon <korivernon@nyu.edu>
2020-12-08 01:47:13 -0500

    latest

---
+++ trades.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>GloomBerg Stonks Homepage</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <h1>Welcome to GloomBerg Stonks</h1>
+  <!-- Create a table given we have SQL Code https://stackoverflow.com/questions/17902483/show-values-from-a-mysql-database-table-inside-a-html-table-on-a-webpage -->
+<!-- how do you pass information between different pages so that it doesn't reset???? -->
+  <table style="width:100%">
+  <tr>
+    <th>Security Type</th>
+    <th>Ticker</th>
+    <th>Purchase Price</th>
+    <th>Sale Price</th>
+    <th>Profit</th>
+    <th>Reasoning</th>
+  </tr>
+  <tr>
+    <td>CALL</td>
+    <td>AAPL</td>
+    <td>$350</td>
+    <td>$400</td>
+    <td>$50</td>
+    <td>Stonks only go up</td>
+  </tr>
+  <tr>
+    <td>CALL</td>
+    <td>NIO</td>
+    <td>$90</td>
+    <td>$140</td>
+    <td>$50</td>
+    <td>NIO MOOOOOON</td>
+  </tr>
+</table>
+
+  <br><br>
+  <a href="index.php"><button>Log Out</button></a>
+  <a href="home.php"><button>Home</button></a>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
