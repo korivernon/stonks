8576b43cca7cdfa9e74c303cf9707ab3a1621793
nativevex <vru11ra@gmail.com>
2020-12-11 17:55:13 -0500

    kori wants a commit from me

---
+++ populate-db.sql
@@ -0,0 +1,14 @@
+/* example data here */
+
+/* Organization */
+insert into Organization(compName, Address) values ('JustStonks', '69 rd');
+insert into Organization(compName, Address) values ('OnlyStonks', '360 rd');
+insert into Organization(compName, Address) values ('StonkHub', '420 rd');
+insert into Organization(compName, Address) values ('WSBHedgeHog', '9001 rd');
+
+/* Users */
+
+/* Transaction table populate */
+insert into transaction (Ticker, purchasedPrice, soldPrice, Security_type, Reasoning, orgID) values ('AAPL', 1110, 1130, 'Option', 'Just because', '765'); */
+
+/*insert into transaction values ('AAPL', 110, 120, 'OPTION', 'Apple Moon', */
